642b6f501daf93991e5067897adba9b9a93c2f06
Ivka Dimin <rass219@mail.ru>
2022-03-30 13:24:57 +0700

    phpuzbek2

--- 3003/index.php
+++ 3003/index.php
@@ -1,9 +1,12 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
+session_start();
+if(!isset($_SESSION['auhorized'])){
+	header('Location: login.php');
+}
 require_once 'uzbek.php';
 $user = new User();
-$user->set('aaaaa', 'bbbb', '123');
-$user->saveToSession();
+$user->loadFromSession();
 $user->show();
 
 ?>
--- 3003/uzbek.php
+++ 3003/uzbek.php
@@ -13,6 +13,10 @@ class User{
 		$_SESSION['user_name'] = $this->name;
 		$_SESSION['user_login'] = $this->login;
 		$_SESSION['user_pas'] = $this->pas;
+
+	}
+	public function loadFromSession(){
+		$this->set($_SESSION['user_name'], $_SESSION['user_login'], $_SESSION['user_pas']);
 	}
 	public function authorize($P){
 		if ($P['login'] === $this->login && $P['pas'] === $this->pas){
@@ -32,6 +36,7 @@ class User{
 		echo '<h3>Имя:'.$this->name.'<h3>';
 		echo '<h3>Логин:'.$this->login.'<h3>';
 		echo '<h3>Пароль:'.$this->pas.'<h3>';
+		echo '<a href="logout.php">Выйти</a>';
 	}
 }
 ?>
